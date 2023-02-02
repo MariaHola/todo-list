9dfadd7f8fdf1161b161f62055d99b9847e2fd29
Maria Hola <maaria.hola@gmail.com>
2023-02-02 19:53:58 +0100

    toggle task done

--- JS/script.js
+++ JS/script.js
@@ -24,12 +24,18 @@
             render();
     };
 
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
         
         for (const task of tasks) {
             htmlString += `
             <li ${task.done ? " style=\"text-decoration: line-through\"" : "" }>
+            <button class="js-done">zrobione?</button>
             <button class="js-remove">usuń</button>  
             ${task.content}
             </li>
@@ -43,6 +49,14 @@
         removeButtons.forEach((removeButtons, index) => {
           removeButton.addEventListener("click", () => {
             removeTask(index);
+          });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+          removeButton.addEventListener("click", () => {
+            toggleTaskDone(index);
           });
         });
     };
