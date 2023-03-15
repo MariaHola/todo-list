abec8e5b416eb06d6ad3648a42d6bc309b1899ec
Maria Hola <maaria.hola@gmail.com>
2023-03-15 15:56:27 +0100

    task icon added

--- JS/script.js
+++ JS/script.js
@@ -53,9 +53,9 @@
         for (const task of tasks) {
             htmlString += `
             <li ${task.done ? " style=\"text-decoration: line-through\"" : "" }>
-            <button class="js-done">zrobione?</button>
-            <button class="js-remove">usuń</button>  
+            <button class="js-done">✔</button>
             ${task.content}
+            <button class="js-remove">🗑</button>  
             </li>
             `;
         }
